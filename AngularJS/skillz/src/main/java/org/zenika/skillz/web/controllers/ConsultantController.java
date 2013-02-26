19652c56d5ecf2d940f95f7310067ccdf666cabf
sebastien <seb.brousse@gmail.com>
2013-02-26 13:52:38 +0100

    Gestion de la création d'un consultant Ajout d'un logger pour la gestion d'erreur

--- AngularJS/skillz/src/main/java/org/zenika/skillz/web/controllers/ConsultantController.java
+++ AngularJS/skillz/src/main/java/org/zenika/skillz/web/controllers/ConsultantController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.*;
 import org.zenika.skillz.model.Consultant;
 import org.zenika.skillz.repositories.ConsultantRepository;
 import org.zenika.skillz.web.pages.ConsultantListResource;
+import org.zenika.skillz.web.pages.ConsultantResource;
 
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,23 +42,71 @@ public class ConsultantController {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public @ResponseBody Consultant getById(@PathVariable long id) {
-        return consultantRepository.findById(id);
+    public @ResponseBody ConsultantResource getById(@PathVariable long id) {
+        return new ConsultantResource(consultantRepository.findById(id));
     }
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    public @ResponseBody Consultant create(@RequestBody Consultant consultant) {
-//        long id = consultantIdGenerator.incrementAndGet();
-//        consultant.setId(id);
-        consultantRepository.create(consultant);
-        return consultant;
+    public @ResponseBody ConsultantResource create(@RequestBody ConsultantResource consultantResource) {
+        Consultant consultant = consultantRepository.create(consultantResource.asConsultant());
+        return new ConsultantResource(consultant);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public @ResponseBody void update(@RequestBody ConsultantResource consultant) {
+        LOGGER.debug("Modification du Consultant : {}", consultant);
+        consultantRepository.update(consultant.asConsultant());
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void delete(@PathVariable long id) {
         consultantRepository.remove(id);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    ErrorMessage handleException(Exception ex) {
+
+//        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+//        List<ObjectError> globalErrors = ex.getBindingResult().getGlobalErrors();
+//        List<String> errors = new ArrayList<>(fieldErrors.size() + globalErrors.size());
+        LOGGER.error("Erreur : ",ex);
+        List<String> errors = new ArrayList<String>();
+//        String error;
+//        for (FieldError fieldError : fieldErrors) {
+//            error = fieldError.getField() + ", " + fieldError.getDefaultMessage();
+//            errors.add(error);
+//        }
+//        for (ObjectError objectError : globalErrors) {
+//            error = objectError.getObjectName() + ", " + objectError.getDefaultMessage();
+//            errors.add(error);
+//        }
+        return new ErrorMessage(errors);
+    }
+
+    @XmlRootElement
+    public class ErrorMessage {
+
+        private List<String> errors;
+
+        public ErrorMessage() {
+        }
+
+        public ErrorMessage(List<String> errors) {
+            this.errors = errors;
+        }
+
+        public List<String> getErrors() {
+            return errors;
+        }
+
+        public void setErrors(List<String> errors) {
+            this.errors = errors;
+        }
     }
 
 }
