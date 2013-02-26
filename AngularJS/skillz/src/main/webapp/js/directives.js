a680e85efd26664af60673065b23ce0115483d52
sebastien <seb.brousse@gmail.com>
2013-02-26 13:55:10 +0100

    Ajout d'un directive pour rendre un champs texte éditable par click

--- AngularJS/skillz/src/main/webapp/js/directives.js
+++ AngularJS/skillz/src/main/webapp/js/directives.js
@@ -9,3 +9,40 @@ angular.module('myApp.directives', []).
       elm.text(version);
     };
   }]);
+
+angular.module('myApp.directives', []).directive('contenteditable', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, elm, attrs, ctrl) {
+            // view -> model
+            elm.bind('blur', function() {
+                scope.$apply(function() {
+                    ctrl.$setViewValue(elm.html());
+                });
+            });
+
+            // model -> view
+            ctrl.render = function(value) {
+                elm.html(value);
+            };
+
+            // load init value from DOM
+            ctrl.$setViewValue(elm.html());
+
+            elm.bind('keydown', function(event) {
+                console.log("keydown " + event.which);
+                var esc = event.which == 27,
+                    el = event.target;
+
+                if (esc) {
+                    console.log("esc");
+                    ctrl.$setViewValue(elm.html());
+                    el.blur();
+                    event.preventDefault();
+                }
+
+            });
+
+        }
+    };
+});
